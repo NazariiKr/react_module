2dcc6f74e144bcc51417cb87381959cc1b6f4b5e
NazariiKr <nazarii999kr@gmail.com>
2025-01-09 23:49:49 +0200

    3.1 hw

---
+++ src/components/CartsComponent/CartsComponent.tsx
@@ -0,0 +1,11 @@
+
+
+const CartsComponent = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default CartsComponent;
--- src/components/UserComponent/UserComponent.tsx
+++ src/components/UserComponent/UserComponent.tsx
@@ -1,13 +1,21 @@
 
 import {IUsers} from "../../models/IUsers.ts";
+import {useNavigate} from "react-router";
 type PromiseType={
     user:IUsers
 }
 
 const UserComponent = ({user}:PromiseType) => {
+    const navigate=useNavigate()
+
+    const buttonNavigate=()=>{
+        navigate('/users/' + user.id + '/carts')
+    }
+
     return (
         <div>
             {user.firstName}
+            <button onClick={buttonNavigate}></button>
 
         </div>
     );
